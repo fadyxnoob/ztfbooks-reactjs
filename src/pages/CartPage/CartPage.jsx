ca2844e45c1792e6a5a996c71caaba1921eb3c2a
Yasir Mehboob <115621928+fadyxnoob@users.noreply.github.com>
2025-02-20 01:09:28 +0500

    login set for cart page

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,21 +1,31 @@
 import React, { useEffect, useState } from "react";
-import BookCoverImage from "../../assets/images/BookCoverImage.png";
 import { FaStar } from "react-icons/fa";
 import Button from "../../components/Button/Button";
 import BookCard from "../../components/BookCard/BookCard";
 import { useSelector } from "react-redux";
 import service from "../../API/DBService";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Alert from "../../components/Alert/Alert";
 
 const CartPage = () => {
-
+  const navigate = useNavigate();
   const apiKey = import.meta.env.VITE_GET_APPROVED_BOOKS_API_KEY;
   const [approvedEBooks, setApprovedEBooks] = useState([]);
   const [cartBooks, setCartBooks] = useState([]);
+  const [alert, setAlert] = useState(null);
 
+  // Function to show alert
+  const showAlert = (type, message) => {
+    setAlert({ type, message });
+    setTimeout(() => setAlert(null), 2000); // Hide after 2 seconds
+  };
   const token = localStorage.getItem("userdata").jwtToken;
-
-
+  if (!token) {
+    showAlert("error", "Please Login First! ");
+    setTimeout(() => {
+      navigate("/login");
+    }, 200);
+  }
 
   const products = useSelector((state) => state?.cart?.products || []);
   const totalQuantity = products.reduce(
@@ -43,7 +53,6 @@ const CartPage = () => {
     getApprovedBooks();
   }, []);
 
-
   // Fetch book details
   useEffect(() => {
     const fetchBookDetails = async () => {
@@ -51,10 +60,16 @@ const CartPage = () => {
         products.map(async (book) => {
           try {
             const bookRes = await service.getBookByID(book.id);
-            
+
             const file = bookRes.data.thumbnailFileName;
             const fileURL = await service.getFileByName(file);
-            return { ...book, fileURL, category:bookRes.data.categories.name, author:bookRes.data.author.name, date:bookRes.data.publishDate };
+            return {
+              ...book,
+              fileURL,
+              category: bookRes.data.categories.name,
+              author: bookRes.data.author.name,
+              date: bookRes.data.publishDate,
+            };
           } catch (err) {
             console.error(`Failed to fetch book ${book.id}:`, err);
             return { ...book, fileURL: null };
@@ -69,129 +84,144 @@ const CartPage = () => {
     }
   }, [products]);
   return (
-    <div className="px-5 md:px-20 py-5 bg-[#f4f3f4]">
-      <h1 className="bg-[#F7F8F8] my-3 p-3 rounded-xl text-[#203949] text-3xl font-medium">
-        Cart
-      </h1>
-      <div className="my-10 flex flex-col md:flex-row items-start gap-5 md:gap-[30px]">
-        <div className="w-full md:w-[70%]">
-          {cartBooks.length > 0 ? (
-            cartBooks.map((book) => {
-              return (
-                <div
-                  key={book.id}
-                  className="my-2 drop-shadow-[0_0_4px_rgba(0,0,0,0.25)] md:h-[100px] p-3 rounded-lg flex flex-col md:flex-row items-center w-full bg-white"
-                >
-                  <div className="flex justify-between w-full md:w-auto overflow-hidden">
-                    <div className="h-[68px] w-[60px] border border-[#D9D9D9]">
-                      <img src={book.fileURL} alt={book.title} className="size-full object-contain"/>
-                    </div>
-                    <div className="md:hidden">
-                      <h3 className="text-xl font-medium text-[#203949]">
-                        {book?.name}
-                      </h3>
-                      <div className="flex items-center gap-2 mt-2">
-                        <p className="text-[#7C7C7C] text-sm font-normal">
-                          {book.rating || "4.0"}
-                        </p>
-                        <FaStar className="text-[#FFCC68]" />
+    <>
+      {alert && (
+        <Alert
+          type={alert.type}
+          message={alert.message}
+          onClose={() => setAlert(null)}
+        />
+      )}
+      <div className="px-5 md:px-20 py-5 bg-[#f4f3f4]">
+        <h1 className="bg-[#F7F8F8] my-3 p-3 rounded-xl text-[#203949] text-3xl font-medium">
+          Cart
+        </h1>
+        <div className="my-10 flex flex-col md:flex-row items-start gap-5 md:gap-[30px]">
+          <div className="w-full md:w-[70%]">
+            {cartBooks.length > 0 ? (
+              cartBooks.map((book) => {
+                return (
+                  <div
+                    key={book.id}
+                    className="my-2 drop-shadow-[0_0_4px_rgba(0,0,0,0.25)] md:h-[100px] p-3 rounded-lg flex flex-col md:flex-row items-center w-full bg-white"
+                  >
+                    <div className="flex justify-between w-full md:w-auto overflow-hidden">
+                      <div className="h-[68px] w-[60px] border border-[#D9D9D9]">
+                        <img
+                          src={book.fileURL}
+                          alt={book.title}
+                          className="size-full object-contain"
+                        />
+                      </div>
+                      <div className="md:hidden">
+                        <h3 className="text-xl font-medium text-[#203949]">
+                          {book?.name}
+                        </h3>
+                        <div className="flex items-center gap-2 mt-2">
+                          <p className="text-[#7C7C7C] text-sm font-normal">
+                            {book.rating || "4.0"}
+                          </p>
+                          <FaStar className="text-[#FFCC68]" />
+                        </div>
                       </div>
                     </div>
-                  </div>
 
-                  <div className="w-full p-2 flex flex-wrap items-center justify-between">
-                    <div className="hidden md:block">
-                      <h3 className="text-xl font-medium text-[#203949]">
-                        {book.name}
-                      </h3>
-                      <div className="flex items-center gap-2 mt-2">
-                        <p className="text-[#7C7C7C] text-sm font-normal">
-                          {book.rating || "4.0"}
+                    <div className="w-full p-2 flex flex-wrap items-center justify-between">
+                      <div className="hidden md:block">
+                        <h3 className="text-xl font-medium text-[#203949]">
+                          {book.name}
+                        </h3>
+                        <div className="flex items-center gap-2 mt-2">
+                          <p className="text-[#7C7C7C] text-sm font-normal">
+                            {book.rating || "4.0"}
+                          </p>
+                          <FaStar className="text-[#FFCC68]" />
+                        </div>
+                      </div>
+                      <div className="">
+                        <h3 className="text-xl font-medium text-[#203949]">
+                          Author
+                        </h3>
+                        <p className="mt-2 text-[#7C7C7C] text-sm font-normal">
+                          {book?.author || "Unknown"}
                         </p>
-                        <FaStar className="text-[#FFCC68]" />
+                      </div>
+                      <div className="">
+                        <h3 className="text-xl font-medium text-[#203949]">
+                          Type
+                        </h3>
+                        <p className="mt-2 text-[#7C7C7C] text-sm font-normal">
+                          {book.category || "Ebook"}
+                        </p>
+                      </div>
+                      <div className="">
+                        <h3 className="text-xl font-medium text-[#203949]">
+                          Price
+                        </h3>
+                        <p className="mt-2 text-[#7C7C7C] text-sm font-normal">
+                          ${book.price}
+                        </p>
+                      </div>
+                      <div>
+                        <p className="text-[#7C7C7C] text-sm font-normal">
+                          {book.date || "N/A"}
+                        </p>
                       </div>
                     </div>
-                    <div className="">
-                      <h3 className="text-xl font-medium text-[#203949]">
-                        Author
-                      </h3>
-                      <p className="mt-2 text-[#7C7C7C] text-sm font-normal">
-                        {book?.author || "Unknown"}
-                      </p>
-                    </div>
-                    <div className="">
-                      <h3 className="text-xl font-medium text-[#203949]">
-                        Type
-                      </h3>
-                      <p className="mt-2 text-[#7C7C7C] text-sm font-normal">
-                        {book.category || "Ebook"}
-                      </p>
-                    </div>
-                    <div className="">
-                      <h3 className="text-xl font-medium text-[#203949]">
-                        Price
-                      </h3>
-                      <p className="mt-2 text-[#7C7C7C] text-sm font-normal">
-                        ${book.price}
-                      </p>
-                    </div>
-                    <div>
-                      <p className="text-[#7C7C7C] text-sm font-normal">
-                        {book.date || "N/A"}
-                      </p>
-                    </div>
                   </div>
+                );
+              })
+            ) : (
+              <p className="text-center text-gray-600">Your cart is empty.</p>
+            )}
+          </div>
+
+          <div className="w-full md:w-[30%]">
+            <div className="rounded-lg bg-white p-3">
+              <h4 className="text-center text-[#7C7C7C] text-lg font-bold">
+                Order Summary
+              </h4>
+              <div className="mt-5">
+                <div className="flex justify-between items-center">
+                  <p className="text-[#7C7C7C] text-lg font-medium">Quantity</p>
+                  <p className="text-[#203949] text-lg font-medium">
+                    {totalQuantity}
+                  </p>
                 </div>
-              );
-            })
-          ) : (
-            <p className="text-center text-gray-600">Your cart is empty.</p>
-          )}
-        </div>
-
-        <div className="w-full md:w-[30%]">
-          <div className="rounded-lg bg-white p-3">
-            <h4 className="text-center text-[#7C7C7C] text-lg font-bold">
-              Order Summary
-            </h4>
-            <div className="mt-5">
-              <div className="flex justify-between items-center">
-                <p className="text-[#7C7C7C] text-lg font-medium">Quantity</p>
-                <p className="text-[#203949] text-lg font-medium">
-                  {totalQuantity}
-                </p>
-              </div>
-              <div className="flex justify-between items-center mt-3">
-                <p className="text-[#7C7C7C] text-lg font-medium">Price/unit</p>
-                <p className="text-[#203949] text-lg font-medium">
-                  ${averageUnitPrice}
-                </p>
-              </div>
-              <div className="flex justify-between items-center mt-3 border-t border-[#EFEFEF] pt-3">
-                <p className="text-[#203949] text-lg font-medium">Total</p>
-                <p className="text-[#203949] text-lg font-medium">
-                  ${totalPrice}
-                </p>
+                <div className="flex justify-between items-center mt-3">
+                  <p className="text-[#7C7C7C] text-lg font-medium">
+                    Price/unit
+                  </p>
+                  <p className="text-[#203949] text-lg font-medium">
+                    ${averageUnitPrice}
+                  </p>
+                </div>
+                <div className="flex justify-between items-center mt-3 border-t border-[#EFEFEF] pt-3">
+                  <p className="text-[#203949] text-lg font-medium">Total</p>
+                  <p className="text-[#203949] text-lg font-medium">
+                    ${totalPrice}
+                  </p>
+                </div>
               </div>
             </div>
+            {/* Checkout button */}
+            <Button classNames="text-white rounded-3xl bg-[#01447E] mt-10 w-full py-3 text-xl font-medium">
+              Checkout
+            </Button>
           </div>
-          {/* Checkout button */}
-          <Button classNames="text-white rounded-3xl bg-[#01447E] mt-10 w-full py-3 text-xl font-medium">
-            Checkout
-          </Button>
         </div>
-      </div>
 
-      {/* Recent books section */}
-      <section className="my-10">
-        <h4 className="text-black text-lg text-center md:text-start font-medium">
-          Recent Ebooks
-        </h4>
-        <div className="flex mt-10 flex-wrap items-center justify-center md:justify-start gap-5">
-          <BookCard books={approvedEBooks} />
-        </div>
-      </section>
-    </div>
+        {/* Recent books section */}
+        <section className="my-10">
+          <h4 className="text-black text-lg text-center md:text-start font-medium">
+            Recent Ebooks
+          </h4>
+          <div className="flex mt-10 flex-wrap items-center justify-center md:justify-start gap-5">
+            <BookCard books={approvedEBooks} />
+          </div>
+        </section>
+      </div>
+    </>
   );
 };
 
