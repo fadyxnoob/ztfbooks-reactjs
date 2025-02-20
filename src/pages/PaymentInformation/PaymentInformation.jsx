e96d0eac41e3889bae25e747618b1411f5ca423e
Yasir Mehboob <115621928+fadyxnoob@users.noreply.github.com>
2025-02-20 22:46:35 +0500

    payment method fix

--- src/pages/PaymentInformation/PaymentInformation.jsx
+++ src/pages/PaymentInformation/PaymentInformation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PreferenceButton from "../../components/PreferenceButton/PreferenceButton";
 import { BsCreditCard } from "react-icons/bs";
 import { FaPlus } from "react-icons/fa6";
